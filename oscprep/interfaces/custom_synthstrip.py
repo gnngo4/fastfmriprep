159b0a2d23db6e1d0711d451d029a5d9485b99ad
gnngo4 <gnvngo@gmail.com>
2022-12-25 17:31:39 -0500

    fix synthstrip cmd bug

--- oscprep/interfaces/custom_synthstrip.py
+++ oscprep/interfaces/custom_synthstrip.py
@@ -6,6 +6,8 @@ from nipype.interfaces.base import (
     traits,
 )
 
+import os
+
 class SynthStripInputSpec(CommandLineInputSpec):
     in_file = File(exists=True, mandatory=True, argstr='-i %s', position=0, desc='input image')
     out_file = File(argstr='-o %s', position=1, desc='output skull-stripped image')
@@ -17,13 +19,15 @@ class SynthStripOutputSpec(TraitedSpec):
     mask_file = File(desc='output binary mask of skull-stripped image (if generated)')
 
 class SynthStrip(CommandLine):
-    _cmd = 'mri_synthstrip'
+    _cmd = 'fspython /opt/synthstrip/mri_synthstrip'
     input_spec = SynthStripInputSpec
     output_spec = SynthStripOutputSpec
 
     def _list_outputs(self):
 
-    return {
-        'out_file': self.inputs.out_file,
-        'mask_file': self.inputs.mask_file
-    }
+        _outputs = {
+            'out_file': os.path.abspath(self.inputs.out_file),
+            'mask_file': os.path.abspath(self.inputs.mask_file)
+        }
+
+        return _outputs
